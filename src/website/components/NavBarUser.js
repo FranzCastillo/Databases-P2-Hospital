e7aea8204bf77511f7d990d384786b9ef65022aa
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2023-04-17 14:50:36 -0600

    NavBar Updated
    Cerrar sesión

--- src/website/components/NavBarUser.js
+++ src/website/components/NavBarUser.js
@@ -1,4 +1,14 @@
+import { supabase } from "../../supabase/client";
+import {useNavigate} from "react-router-dom";
+
 function NavBarUser() { //Navbar del user
+    const navigate = useNavigate();
+    
+    function redirectPage() {
+        navigate('/login');
+        supabase.auth.signOut();
+    }
+
     return (
         <nav className="navbar">
             <ul>
@@ -7,7 +17,7 @@ function NavBarUser() { //Navbar del user
                 <li><a href="/expedientes"> Expedientes </a></li>
                 <li><a href="/bitacora"> Bitácora </a></li>
                 <li><a href="/expedientes/nuevo"> Nueva Consulta </a></li>
-                <li><a href="/signout">Cerrar sesión</a></li>
+                <li><a href="#" onClick={redirectPage} >Cerrar sesión</a></li>
             </ul>
         </nav>
     );
